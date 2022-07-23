bdc3034baad4d818c327a8bebb8f85d5d3244f9d
Ivan Khakhanov <ikhakhanov@ptsecurity.ru>
2022-07-23 15:31:01 +0300

    update teams struct

--- models/teams.go
+++ models/teams.go
@@ -14,11 +14,13 @@ type Team struct {
 	Login     string             `bson:"login"`
 	Hash      string             `bson:"hash"`
 	Visible   bool               `bson:"visible"`
-	Blocked   bool               `bson: blocked`
+	Blocked   bool               `bson:"blocked"`
 }
 type TeamInfo struct {
 	CreatedAt time.Time `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
 	Name      string    `bson:"name" json:"name"`
 	Login     string    `bson:"login" json:"login"`
+	Visible   bool      `bson:"visible" json:"visible"`
+	Blocked   bool      `bson:"blocked" json:"blocked"`
 }
