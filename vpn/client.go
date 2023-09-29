ed7b263989574bf133d4a69a9c1ef67b5b414e80
Ivan Hahanov <ivanhahanov13@gmail.com>
2023-09-29 19:59:58 +0300

    add path

--- vpn/client.go
+++ vpn/client.go
@@ -15,7 +15,7 @@ type VpnClient struct {
 // Generator function for initializate Vpn Client
 // It returns Vpn client Object
 func CreateVpnCLient(login string, args ...string) *VpnClient {
-	var vpnUrl = fmt.Sprintf("http://%s:%s/", os.Getenv("OVPN_HOST"), os.Getenv("OVPN_PORT"))
+	var vpnUrl = fmt.Sprintf("http://%s:%s%s", os.Getenv("OVPN_HOST"), os.Getenv("OVPN_PORT"), os.Getenv("OVPN_LISTEN_BASE_URL"))
 	password := ""
 
 	if len(args) > 0 {
